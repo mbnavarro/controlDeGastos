83452bed7ac6bc7f60159fa184e06ee9a82cea1f
Melisa Navarro <melisabnavarro@gmail.com>
2025-08-25 11:19:38 -0300

    Se suma el clic al boton agregar movimiento

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Redirección del boton "Ingrese Movimiento"
+const boton = document.getElementById('btn-movimiento');
+
+boton.addEventListener('click', function() {
+    window.location.href = './pages/movimientos.html';
+});
+
+
